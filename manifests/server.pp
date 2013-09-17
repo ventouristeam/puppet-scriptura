076e0946d95b2cb62cddd1bc704fab4bb69da799
Dieter De Meyer <dieterdemeyer@gmail.com>
2013-09-17 15:30:11 +0200

    Implemented functionality for configuration.xml in /data folder

--- manifests/server.pp
+++ manifests/server.pp
@@ -1,4 +1,17 @@
-class scriptura::server($version=undef, $key_server=undef, $server_category='production') {
+class scriptura::server(
+  $version=undef,
+  $key_server=undef,
+  $server_category='production',
+  $mail_host=undef,
+  $core_min_threads='20',
+  $core_max_threads='20',
+  $cache_max_memory_entries='100',
+  $cache_max_disk_space='500',
+  $cache_max_disk_entries='200',
+  $fonts_dir='/data/scriptura/Fonts',
+  $logger_max_file_index='50',
+  $logger_max_file_size='2048'
+) {
 
   include stdlib
 
@@ -17,13 +30,27 @@ class scriptura::server($version=undef, $key_server=undef, $server_category='pro
     fail('Class[Scriptura::Server]: parameter server_category must be auto, production, backup, test or development')
   }
 
+  if ! $mail_host {
+    fail('Class[Scriptura::Server]: parameter mail_host must be provided')
+  }
+
   class { 'scriptura::server::package':
     version => $version
   }
 
   class { 'scriptura::server::config':
-    key_server      => $key_server,
-    server_category => $server_category
+    version                  => $version,
+    key_server               => $key_server,
+    server_category          => $server_category,
+    mail_host                => $mail_host,
+    core_min_threads         => $core_min_threads,
+    core_max_threads         => $core_max_threads,
+    cache_max_memory_entries => $cache_max_memory_entries,
+    cache_max_disk_space     => $cache_max_disk_space,
+    cache_max_disk_entries   => $cache_max_disk_entries,
+    fonts_dir                => $fonts_dir,
+    logger_max_file_index    => $logger_max_file_index,
+    logger_max_file_size     => $logger_max_file_size
   }
 
   include scriptura::server::service
